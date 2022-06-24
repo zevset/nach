70349936208843835a800653051878dd69823cd5
zevset <80012592+zevset@users.noreply.github.com>
2022-06-24 09:24:21 -0500

    Respuesta Response

--- nach/src/main/java/com/nach/practica/nach/api/EmployeeApi.java
+++ nach/src/main/java/com/nach/practica/nach/api/EmployeeApi.java
@@ -8,6 +8,7 @@ import javax.xml.ws.Response;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,23 +26,16 @@ public class EmployeeApi {
 	private static final Logger LOGGER = LoggerFactory.getLogger(EmployeeApi.class);
 	@Autowired
 	private EmployeeService service;
-	
-	private static final String NAMESPACE = "http://recibecfdi.sat.gob.mx";
 
     @RequestMapping(value="/CreateEmployee", method=RequestMethod.POST)
 	public ResponseEntity createEmployee( @RequestBody RequestEmployee req) {		
     	
     	LOGGER.info(req.getGender_id()+" "+ req.getJob_id()+" "+ req.getName()+" "+  req.getLast_name()+" "+  req.getBirthdate().toString());
     	int res = service.createEmployee(req.getGender_id(), req.getJob_id(), req.getName(), req.getLast_name(), req.getBirthdate().toString());
-    	ResponseEntity response = null;
-    	if(res >0) {
-    		Optional op = Optional.of(new ResponseCreate(res, true));
-    		ResponseEntity.of(op).ok();
-    	}else {
-    		Optional op = Optional.of(new ResponseCreate(null, false));
-    		ResponseEntity.of(op).internalServerError();
-    	}
-    	return response;
+    	if(res >0)
+    		return new ResponseEntity<>(new ResponseCreate(res, true), HttpStatus.OK);
+
+    	return new ResponseEntity<>(new ResponseCreate(null, false), HttpStatus.INTERNAL_SERVER_ERROR);
 	}
 
     @RequestMapping(value="/AssingHours", method=RequestMethod.POST)
@@ -49,15 +43,10 @@ public class EmployeeApi {
     	LOGGER.info(req.getEmployee_id()+" "+ req.getWorked_hours()+" "+ req.getWorked_date());
     	int res = service.assignHoursToEmployee(req.getEmployee_id(),req.getWorked_hours(), req.getWorked_date());
     	
-    	ResponseEntity response = null;
-    	if(res >0) {
-    		Optional op = Optional.of(new ResponseCreate(res, true));
-    		ResponseEntity.of(op).ok();
-    	}else {
-    		Optional op = Optional.of(new ResponseCreate(null, false));
-    		ResponseEntity.of(op).internalServerError();
-    	}
-    	return response;
+    	if(res >0)
+    		return new ResponseEntity<>(new ResponseCreate(res, true), HttpStatus.OK);
+
+    	return new ResponseEntity<>(new ResponseCreate(null, false), HttpStatus.INTERNAL_SERVER_ERROR);
 	}
 
     @RequestMapping(value="/ListEmployees", method=RequestMethod.GET)
